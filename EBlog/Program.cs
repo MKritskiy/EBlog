399b68df59b7ebb57b4383555d640a38dda771c3
MKritskiy <bolshoy.homyak@gmail.com>
2023-12-15 03:47:14 +0300

    htps

--- EBlog/Program.cs
+++ EBlog/Program.cs
@@ -31,6 +31,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
